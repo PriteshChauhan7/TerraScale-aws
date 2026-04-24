fafca168496c21572774f87c64794c12b9a9c81c
PriteshChauhan7 <Priteshchauhan8265@gmail.com>
2026-04-25 03:02:04 +0530

    Fix ALB subnets and cleanup

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -31,7 +31,7 @@ resource "aws_lb" "main" {
     internal           = false
     load_balancer_type = "application"
     security_groups    = [aws_security_group.alb_sg.id]
-    subnets            = [var.public_subnet_1_id]
+    subnets            = [var.public_subnet_1_id, var.public_subnet_2_id]
 
     tags = {
         Name = "${var.project_name}-alb"
