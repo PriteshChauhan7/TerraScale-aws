c591cb698fb3c14050cc9c21a2a47d17c76f0c64
PriteshChauhan7 <Priteshchauhan8265@gmail.com>
2026-04-25 02:05:34 +0530

    fix subnet output names and sg label

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -8,17 +8,17 @@ output "public_subnet_1" {
     value = aws_subnet.public_1.id
 }
 
-output "public_subnet_2" {
+output "public_subnet_2_id" {
     description = "ID of public subnet 2"
     value = aws_subnet.public_2.id
 }
 
-output "private_subnet_1" {
+output "private_subnet_1_id" {
     description = "ID of private subnet 1"
     value = aws_subnet.private_1.id
 }
 
-output "private_subnet_2" {
+output "private_subnet_2_id" {
     description = "ID of private subnet 2"
     value = aws_subnet.private_2.id
 }
